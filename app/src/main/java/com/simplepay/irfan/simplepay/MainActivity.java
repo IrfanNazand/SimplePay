cca85ef363c56aea529aaffbc199fdcdf800fdd2
IrfanNazand <irfan.nazand@live.com>
2018-09-18 20:26:58 +0200

    setup

--- app/src/main/java/com/simplepay/irfan/simplepay/MainActivity.java
+++ app/src/main/java/com/simplepay/irfan/simplepay/MainActivity.java
@@ -2,8 +2,14 @@ package com.simplepay.irfan.simplepay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction (View view){
+        Log.i("HEi", "Trykket på knappen");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

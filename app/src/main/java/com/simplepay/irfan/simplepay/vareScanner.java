bf69bb2312a7cf045ad32cd63a60ec2d1277b792
IrfanNazand <irfan.nazand@live.com>
2018-09-20 01:16:32 +0200

    Svart Camera skjerm

--- app/src/main/java/com/simplepay/irfan/simplepay/vareScanner.java
+++ app/src/main/java/com/simplepay/irfan/simplepay/vareScanner.java
@@ -19,12 +19,14 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
 
+
 public class vareScanner extends AppCompatActivity {
 
     SurfaceView surfaceView;
     CameraSource cameraSource;
     TextView textView;
     BarcodeDetector barcodeDetector;
+
 
 
     @Override
@@ -39,29 +41,24 @@ public class vareScanner extends AppCompatActivity {
 
         cameraSource = new CameraSource.Builder(this, barcodeDetector).setRequestedPreviewSize(640, 480).build();
 
-
+        // Adder EVENT
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
 
                 if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
                     return;
                 }
-
                 try
                 {
                     cameraSource.start(holder);
-                }catch(IOException e) {
+
+                }catch(IOException e)
+                {
                     e.printStackTrace();
                 }
             }
+
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
@@ -70,9 +67,7 @@ public class vareScanner extends AppCompatActivity {
 
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
-
                 cameraSource.stop();
-
             }
         });
 
@@ -84,20 +79,19 @@ public class vareScanner extends AppCompatActivity {
 
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
-
-                if (qrCodes.size() !=0)
+                 final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+                 if (qrCodes.size() !=0)
                 {
                     textView.post(new Runnable() {
                         @Override
                         public void run() {
-                            Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                            //Lager Vibrate
+                            Vibrator vibrator = (Vibrator)getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
                             vibrator.vibrate(1000);
                             textView.setText(qrCodes.valueAt(0).displayValue);
                         }
                     });
                 }
-
             }
         });
     }
